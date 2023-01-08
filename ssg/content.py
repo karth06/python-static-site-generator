1bd5f7f4c449e5aec22e15c72af66fe196fd4469
bunty <karthik.patel06@gmail.com>
2023-01-08 19:16:16 +0530

    Completed Parser Content Module

--- ssg/content.py
+++ ssg/content.py
@@ -4,16 +4,16 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        #return cls(metadata, content)
+        metadata=load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
-    def __init__(self,metadata, content):
+    def __init__(self, metadata, content):
         self.data = metadata
         self.data["content"] = content
 
@@ -26,14 +26,21 @@ class Content(Mapping):
         return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self):
+    def type(self, type):
         self.data["type"] = type
 
-    def __getitem__(self):
-        return self.data[""]
+    def __getitem__(self, key):
+        return self.data[key]
 
+    def __iter__(self):
+        return self.data.__iter__()
 
+    def __len__(self):
+        return self.data.__len__()
 
-
-
-
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key!="content":
+                data[key]=value
+        return str(data)
